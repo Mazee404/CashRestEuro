00239fa9b8dad07e70fef56628dc28532e3f6ea6
Martin Maze <maze@mailfence.com>
2022-02-21 18:41:07 +0100

    field mod

--- src/Calculation.java
+++ src/Calculation.java
@@ -4,6 +4,9 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Calculation {
+
+	NumberFormat n = NumberFormat.getCurrencyInstance(Locale.ITALY);
+	BigDecimal change1;
 	
 	public String return1(BigDecimal bills, BigDecimal clients) {
 		
@@ -11,41 +14,37 @@ public class Calculation {
 		BigDecimal bill =  bills;		
 		BigDecimal client =  clients;	
 		BigDecimal HUNDRED = new BigDecimal(100);	
-		BigDecimal change1 = bill.subtract(client);
+		change1 = bill.subtract(client);
 		BigDecimal change2 = change1.multiply(HUNDRED);	    
-
-		BigDecimal[] vrednosti = {new BigDecimal(1), new BigDecimal(2), new BigDecimal(5), 
+		
+		BigDecimal[] money = {new BigDecimal(1), new BigDecimal(2), new BigDecimal(5), 
 				    			  new BigDecimal(10), new BigDecimal(20), new BigDecimal(50),
 					  			  new BigDecimal(100), new BigDecimal(200), new BigDecimal(500), 
 								  new BigDecimal(1000), new BigDecimal(2000), new BigDecimal(5000), 
 								  new BigDecimal(10000), new BigDecimal(20000), new BigDecimal(50000)};
 		
-		int i = vrednosti.length-1; 
+		int i = money.length-1; 
 		BigDecimal num;
 				
 		while (change2.compareTo(BigDecimal.ZERO) > 0 && i >= 0) {				
-			num = change2.divide(vrednosti[i],0,RoundingMode.DOWN);
+			num = change2.divide(money[i],0,RoundingMode.DOWN);
 			if (num.compareTo(BigDecimal.ZERO) != 0) { 						
 				if (change2.compareTo(BigDecimal.valueOf(100)) >= 0) {
-					x += ("\t"+(vrednosti[i].divide(HUNDRED))+"€ = "+num+"\n"); 
+					x += ("\t"+(money[i].divide(HUNDRED))+" E  =  "+num+"\n"); 
 				} else if (change2.compareTo(BigDecimal.valueOf(10)) >= 0){
-					x += ("\t"+(vrednosti[i].divide(HUNDRED))+"0c = "+num+"\n");
+					x += ("\t"+(money[i].divide(HUNDRED))+"0 c  =  "+num+"\n");
 				} else  if (change2.compareTo(BigDecimal.valueOf(1)) >= 0){
-					x += ("\t"+(vrednosti[i].divide(HUNDRED))+"c = "+num+"\n");
+					x += ("\t"+(money[i].divide(HUNDRED))+" c  =  "+num+"\n");
 				}				
-				change2 = change2.subtract(num.multiply(vrednosti[i]));
+				change2 = change2.subtract(num.multiply(money[i]));
 			}
 			i--;
 		}		
 		return x;
 	}
 			
-	public String return2(BigDecimal bills, BigDecimal clients) {
-		BigDecimal bill = bills;	
-		BigDecimal client = clients;	
-		BigDecimal change = bill.subtract(client);		
-		NumberFormat n = NumberFormat.getCurrencyInstance(Locale.ITALY); 
-	    String s = n.format(change);	    
+	public String return2() {		 
+	    String s = n.format(this.change1);	    
 		return s;
 	}
 }
--- src/Interface.java
+++ src/Interface.java
@@ -4,6 +4,8 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.math.BigDecimal;
@@ -18,17 +20,21 @@ public class Interface {
 
 	private JFrame frame;
 	private JPanel panel;
+
+	private JLabel lblTitle;
 	private JLabel lblString;
 	private JLabel lblString2;
+
 	public JTextField textField;
-	public JTextField textFiled2;
+	public JTextField textField2;
 	private JLabel lblButtonClick; 
+
 	private JLabel label;
 	private JLabel label2;
-	private JLabel label3;
-	private JLabel label4;
-	private JTextArea result;
-	private JLabel result2;
+	private JLabel lblResult;
+	
+	private JTextArea result2;
+	private JLabel result;
 	private Calculation calculation;
 
 	Interface() {
@@ -36,13 +42,13 @@ public class Interface {
 		this.calculation = new Calculation();
 	}
 
-	public void see() {
+	public void open() {
 		frame.setVisible(true);
 	}
 
 	private void view() { 
 		frame = new JFrame("The Rest"); 
-		frame.setSize(300, 430);
+		frame.setSize(300, 440);
 		frame.setLocation(700, 300);
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent event) {
@@ -50,17 +56,30 @@ public class Interface {
 			}
 		});
 
-		this.lblString = new JLabel("Enter how much is Bill:"); 
-		this.textField = new JTextField(10); 
+		Color myColor = new Color(154, 208, 236);
 
-		this.lblString2 = new JLabel("Enter how much is Cash:");
-		this.textFiled2 = new JTextField(10);
+		lblTitle = new JLabel("         D°Rest         ");
+		lblTitle.setFont(new Font(Font.SANS_SERIF,Font.BOLD,25));	
 
-		this.lblButtonClick = new JLabel();
+		lblString = new JLabel("Bill amount:"); 
+		lblString.setFont(new Font(Font.SANS_SERIF,Font.BOLD,18));
 		
-		result = new JTextArea();	
-		result2 = new JLabel();
+		textField = new JTextField(7); 
+		label = new JLabel();
 
+		lblString2 = new JLabel("Cash amount:");
+		lblString2.setFont(new Font(Font.SANS_SERIF,Font.BOLD,18));
+		
+		textField2 = new JTextField(7);
+		label2 = new JLabel();
+
+		lblButtonClick = new JLabel();
+
+		lblResult = new JLabel("Cash to return : ");
+		result = new JLabel();
+
+		result2 = new JTextArea();	
+		
 		JButton button = new JButton("Enter"); 
 		button.setPreferredSize(new Dimension(230,30));
 		
@@ -71,13 +90,13 @@ public class Interface {
 				label.setText(textField.getText().toString());
 				
 				lblButtonClick.setText("");
-				label2.setText(textFiled2.getText().toString());
+				label2.setText(textField2.getText().toString());
 
 				BigDecimal bill = new BigDecimal(textField.getText().toString());
-				BigDecimal client = new BigDecimal(textFiled2.getText().toString());
+				BigDecimal client = new BigDecimal(textField2.getText().toString());
 
-				String cashBack = calculation.return1(bill, client);
-				String cashBack2 = calculation.return2(bill, client);
+				String cashBack2 = calculation.return1(bill, client);
+				String cashBack = calculation.return2();
 				
 				result.setText(cashBack);
 				result2.setText(cashBack2);
@@ -85,45 +104,67 @@ public class Interface {
 		});
 
 		panel = new JPanel();
-		panel.setLayout(new FlowLayout()); 
-		Color myColor = new Color(154, 208, 236);
+		panel.setLayout(new FlowLayout());
 		panel.setBackground(myColor);
-		result.setBackground(myColor);
 
-		label4 = new JLabel("     D°Rest     ");
-		label4.setFont(new Font(Font.SANS_SERIF,Font.BOLD,25));	
-		panel.add(label4);
-		panel.add(lblString); 
-		 
-		panel.add(textFiled2);
-		panel.add(lblString2);
+		panel.add(lblTitle);
+
+		panel.add(lblString); 		 
 		panel.add(textField);
+		
+		panel.add(lblString2);
+		panel.add(textField2);	
 
 		panel.add(button);
 		panel.add(lblButtonClick);
-		
-		label = new JLabel();
-		label2 = new JLabel();
-		label3 = new JLabel("Vsota za vracilo : ");
-			
-		panel.add(label3);
-		panel.add(result2);
+					
+		panel.add(lblResult);
 		panel.add(result);
-		
-		result.setFont(new Font(Font.SANS_SERIF,Font.BOLD,12));
-		result.setColumns(20);
-		result.setRows(15);
-		result.setLineWrap(true);	
-		result.setWrapStyleWord(true);
-		result.setEditable(false);
-		
-		frame.setResizable(false);
+
+		result2.setBackground(myColor);
+		result2.setFont(new Font(Font.SANS_SERIF,Font.BOLD,12));
+		result2.setColumns(20);
+		result2.setRows(15);
+		result2.setLineWrap(true);	
+		result2.setWrapStyleWord(true);
+		result2.setEditable(false);		
+		panel.add(result2);
+
+		textField.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {
+			  char c = e.getKeyChar();
+			  if (!(Character.isDigit(c) ||
+				 (c == KeyEvent.VK_BACK_SPACE) ||
+				 (c == KeyEvent.VK_DELETE)||
+				 (c == KeyEvent.VK_PERIOD))) {
+				   e.consume();
+				 }
+			}
+			public void keyPressed(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {}
+		});
+
+		textField2.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {
+			  char c = e.getKeyChar();
+			  if (!(Character.isDigit(c) ||
+				 (c == KeyEvent.VK_BACK_SPACE) ||
+				 (c == KeyEvent.VK_DELETE)||
+				 (c == KeyEvent.VK_PERIOD))) {
+				   e.consume();
+				 }
+			}
+			public void keyPressed(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {}
+		});
+
 		frame.add(panel); 
+		frame.setResizable(false);	
 		frame.setVisible(false);
 	}
 
 	public static void main(String[] args) {
 		Interface program = new Interface(); 
-		program.see();
+		program.open();
 	}
 }
